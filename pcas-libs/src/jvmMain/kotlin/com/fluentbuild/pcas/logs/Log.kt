1229c132991ad547753e35ab29e378f6083042e7
elvis10ten <elvis.chidera@gmail.com>
2020-10-05 17:12:35 -0400

    Improved logs

--- pcas-libs/src/jvmMain/kotlin/com/fluentbuild/pcas/logs/Log.kt
+++ pcas-libs/src/jvmMain/kotlin/com/fluentbuild/pcas/logs/Log.kt
@@ -6,6 +6,7 @@ import kotlin.reflect.KFunction
 actual class Log actual constructor(private val className: String) {
 
 	private val tree = Timber.tagged(className)
+	private val richLog = RichLog
 
 	actual fun debug(function: KFunction<*>, vararg args: Any) {
 		val arguments = args.joinToString { it.toString() }
@@ -18,22 +19,22 @@ actual class Log actual constructor(private val className: String) {
 
 	actual fun info(message: () -> String) {
 		tree.info(message = message)
-		RichLog.append(className, message, RichLog.Type.INFO)
+		richLog.append(className, message, RichLog.Type.INFO)
 	}
 
 	actual fun warn(message: () -> String) {
 		tree.warn(message = message)
-		RichLog.append(className, message, RichLog.Type.WARN)
+		richLog.append(className, message, RichLog.Type.WARN)
 	}
 
 	actual fun error(message: () -> String) {
 		tree.error(message = message)
-		RichLog.append(className, message, RichLog.Type.ERROR)
+		richLog.append(className, message, RichLog.Type.ERROR)
 	}
 
 	actual fun error(throwable: Throwable, message: () -> String) {
 		tree.error(throwable, message)
-		RichLog.append(className, message, RichLog.Type.ERROR)
-		RichLog.append(className, { throwable.toString() }, RichLog.Type.ERROR)
+		richLog.append(className, message, RichLog.Type.ERROR)
+		richLog.append(className, { throwable.toString() }, RichLog.Type.ERROR)
 	}
 }
